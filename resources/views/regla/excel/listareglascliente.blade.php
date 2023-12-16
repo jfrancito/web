48a05eb726ffac93c5a2fed5df98107166d2460a
Jorge Francelli <francito20al@gmail.com>
2023-12-16 09:32:38 -0500

    reporte

--- resources/views/regla/excel/listareglascliente.blade.php
+++ resources/views/regla/excel/listareglascliente.blade.php
@@ -1,7 +1,69 @@
 <html>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style type="text/css">
+        
+h1{
+    text-align: center;
+}
+.subtitulos{
+    font-weight: bold;
+    font-style: italic;
+}
+.titulotabla{
+    background: #4285f4;
+    color: #fff;
+    font-weight: bold;
+}
+.tabladp{
+    background: #bababa;
+    color:#fff;
+}
+.tablaho{
+    background: #37b358;
+    color:#fff;
+}
+.tablamar{
+    background: #4285f4;
+    color:#fff;
+}
+.tablaagrupado{
+    background: #ea4335;
+    color:#fff;
+}
+.negrita{
+    font-weight: bold;
+}
+.center{
+    text-align: center;
+}
+.reportevacadesc{
+        background: #ea4335;
+    color: #fff;
+    font-weight: bold;
+}
+.tablafila2{
+  background: #f5f5f5;
+}
+.tablafila1{
+  background: #ffffff;
+}
+.warning{
+  background-color: #f6c163 !important;
+}
 
-	{!! Html::style('public/css/excel/excel.css') !!}
+/*.vcent { display: table;  }*/
+
+.vcent{ display: table-cell; vertical-align:middle;text-align: center;}
+
+.gris{
+    background: #C8C9CA;
+}
+.blanco{
+  background: #ffffff;
+}
+
+    </style>
+
 
     <!-- titulo -->
     <table>
